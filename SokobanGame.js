eec98ce079f212183b322ec7fbf01c5d178e0d84
Philip Wahlgren <phwahlgren@gmail.com>
2019-12-13 15:53:10 +0100

    some updates

--- SokobanGame.js
+++ SokobanGame.js
@@ -1,7 +1,9 @@
 "use strict";
 var gameGrid = document.getElementById("gameGrid");
-var playerId = 0;
 var boxes = [];
+var playerMoves = [];
+var boxMoves = [];
+var player;
 var moves = 0;
 var seconds = 0;
 var minutes = 0;
@@ -26,12 +28,12 @@ function startGame() {
                 case "B":
                     element.classList.add(Tiles.Space);
                     element.classList.add(Entities.Block);
-                    boxes.push(element.id);
+                    boxes.push(element);
                     break;
                 case "P":
                     element.classList.add(Tiles.Space);
                     element.classList.add(Entities.Character);
-                    playerId = element.id;
+                    player = element;
                     break;
                 default:
                     element.classList.add(Tiles.Space);
@@ -40,17 +42,17 @@ function startGame() {
         }
     }
     document.getElementById("movesText").innerHTML = "Moves: " + moves;
-    document.getElementById("timeText").innerHTML = "Time: " + minutes + " : " + seconds;
+    document.getElementById("timeText").innerHTML = "Time: 00 : 00";
     timer = setInterval(updateTime, 1000);
 }
 
 
 function movePlayer(event) {
     const key = event.keyCode;
-    if (key === 37 || key === 38 || key === 39 || key === 40 || key === 13) {
-        const yPos = playerId.indexOf("y");
-        let x = parseInt(playerId.slice(1, yPos));
-        let y = parseInt(playerId.slice(yPos + 1));
+    if (key === 37 || key === 38 || key === 39 || key === 40 || key === 13 || key === 32) {
+        const yPos = player.id.indexOf("y");
+        let x = parseInt(player.id.slice(1, yPos));
+        let y = parseInt(player.id.slice(yPos + 1));
 
         switch (key) {
             case 37:
@@ -65,11 +67,20 @@ function movePlayer(event) {
             case 40:
                 checkCollision(x, +0, y, + 1);
                 break;
+            case 32:
+                stepBack();
+                break;
             case 13:
+                /*
                 gameGrid.innerHTML = "";
                 while (boxes.length > 0) {
                     boxes.pop();
                 }
+                */
+                boxes.length = [];
+                boxes.length = [];
+                playerMoves.length = [];
+                boxMoves.length = [];
                 moves = 0;
                 seconds = 0;
                 minutes = 0;
@@ -90,12 +101,11 @@ function checkCollision(x, xChange, y, yChange) {
     boxCheck = checkBoxCollision(x, y, nextX, nextY);
 
     if (tileMap01.mapGrid[y][x][0] !== "W" && boxCheck === true) {
-        const currentPlayerPos = document.getElementById(playerId);
-        currentPlayerPos.classList.remove(Entities.Character)
-        playerId = "x" + x + "y" + y;
-        const newPlayerPos = document.getElementById(playerId);
-        newPlayerPos.classList.add(Entities.Character);
-        console.log(newPlayerPos);
+        playerMoves.push(player);
+        player.classList.remove(Entities.Character)
+        player = document.getElementById("x" + x + "y" + y);
+        player.classList.add(Entities.Character);
+        console.log(player);
 
         moves++;
         document.getElementById("movesText").innerHTML = "Moves: " + moves;
@@ -106,25 +116,19 @@ function checkCollision(x, xChange, y, yChange) {
 function checkBoxCollision(x, y, nextX, nextY) {
     let noBoxCollide = true;
     for (let i = 0; i < boxes.length; i++) {
-        if (boxes[i] === "x" + x + "y" + y) {
+        if (boxes[i].id === "x" + x + "y" + y) {
             for (let l = 0; l < boxes.length; l++) {
-                if (boxes[l] === "x" + nextX + "y" + nextY) {
+                if (boxes[l].id === "x" + nextX + "y" + nextY) {
                     noBoxCollide = false;
                     break;
                 }
             }
 
             if (tileMap01.mapGrid[nextY][nextX][0] !== "W" && noBoxCollide === true) {
-                const oldBoxPos = document.getElementById(boxes[i]);
-                boxes[i] = "x" + nextX + "y" + nextY;
-                let goalBox = checkGoalBoxes(oldBoxPos, boxes[i], x, y, nextX, nextY);
-
-                if (goalBox === false) {
-                    oldBoxPos.classList.remove(Entities.Block);
-                    const newBoxPos = document.getElementById(boxes[i]);
-                    newBoxPos.classList.add(Entities.Block);
-                    console.log(newBoxPos);
-                }
+                const oldBox = boxes[i];
+                const newBox = document.getElementById("x" + nextX + "y" + nextY);
+                boxMoves.push([moves, i, oldBox]);
+                moveBoxes(oldBox, i, newBox);
                 return true;
             }
             else {
@@ -136,34 +140,35 @@ function checkBoxCollision(x, y, nextX, nextY) {
     return true;
 }
 
-function checkGoalBoxes(oldBoxPos, boxId, oldX, oldY, nextX, nextY) {
-    if (tileMap01.mapGrid[oldY][oldX][0] === "G") {
-        oldBoxPos.classList.remove(Entities.BlockDone)
+function moveBoxes(oldBox, i, newBox) {
+    if (oldBox.classList.contains("entity-block-goal")) {
+        oldBox.classList.remove(Entities.BlockDone)
     }
     else {
-        oldBoxPos.classList.remove(Entities.Block);
+        oldBox.classList.remove(Entities.Block);
     }
-    if (tileMap01.mapGrid[nextY][nextX][0] === "G") {
+    if (newBox.classList.contains("tile-goal")) {
 
-        const newBoxPos = document.getElementById(boxId);
-        newBoxPos.classList.add(Entities.BlockDone);
-        console.log(newBoxPos);
-
-        
-        return true;
+        newBox.classList.add(Entities.BlockDone);
+        console.log(newBox);
     }
-    return false;
+    else {
+        oldBox.classList.remove(Entities.Block);
+        newBox.classList.add(Entities.Block);
+        console.log(newBox);
+    }
+    boxes[i] = newBox;
 }
 
 function checkWinCondition() {
     let gameWon = true;
-    for (let i = 0; i < boxes.length; i++) {
-        let box = document.getElementById(boxes[i]);
+    boxes.forEach(box => {
         if (!box.classList.contains("entity-block-goal")) {
             gameWon = false;
-            break;
+            return;
         }
-    }
+    });
+
     if (gameWon === true) {
         clearInterval(timer);
         const h1Element = document.createElement("h1");
@@ -173,13 +178,42 @@ function checkWinCondition() {
     }
 }
 
-function updateTime(){
+function stepBack() {
+    if (moves > 0) {
+        player.classList.remove(Entities.Character);
+        player = playerMoves[playerMoves.length - 1];
+        player.classList.add(Entities.Character);
+        playerMoves.pop();
+
+        moves -= 1;
+        document.getElementById("movesText").innerHTML = "Moves: " + moves;
+
+        for (let i = 0; i < boxMoves.length; i++) {
+            if (boxMoves[i][0] === moves) {
+                let id = boxMoves[i][1];
+                moveBoxes(boxes[id], id, boxMoves[i][2]);
+                boxMoves.pop();
+                break;
+            }
+        }
+    }
+}
+
+function updateTime() {
     seconds++;
-    
-    while(seconds >= 60){
+    while (seconds >= 60) {
         minutes++;
         seconds -= 60;
     }
+    let secondsStr = seconds.toString();
+    let minutesStr = minutes.toString();
 
-    document.getElementById("timeText").innerHTML = "Time: " + minutes + " : " + seconds;
+    if (seconds < 10) {
+        secondsStr = "0" + seconds.toString();
+    }
+    if (minutes < 10) {
+        minutesStr = "0" + minutes.toString();
+    }
+
+    document.getElementById("timeText").innerHTML = "Time: " + minutesStr + " : " + secondsStr;
 }
